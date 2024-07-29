d1c763cf8fb255406b6ebd19b60fb8df6f2b2734
Nurdin_Khoirurizka <54714249+Khoirurizka@users.noreply.github.com>
2024-07-29 18:14:02 +0800

    Update botcontrol.cpp

--- src/botcontrol.cpp
+++ src/botcontrol.cpp
@@ -19,8 +19,83 @@ bool turningRed = false;
 int forwardCurve = 10;
 bool forwardRed = false;
 
-int curveJoystick(bool red, int input, double t){
-  int val = 0;
+enum curve {linear=0, exponential=1, logarithmic=2, s_curve=3,parabolic=4,cubic=5,quadratic=6,step=7,sensitivity=8};
+
+// Linear Curve with gradient
+double f_linear(double x, double a = 1, double b = 0) {
+    return a * x + b;
+}
+
+// Exponential Curve with base and scale
+double f_exponential(double x, double a = M_E, double b = 1) {
+    return b * (std::pow(a, x) - 1);
+}
+
+// Logarithmic Curve with base and scale
+double f_logarithmic(double x, double a = M_E, double b = 1) {
+    return b * (std::log(x + 1) / log(a));
+}
+
+// S-Curve (Sigmoid Curve) with steepness and midpoint
+double f_s_curve(double x, double a = 1, double b = 0) {
+    return 1 / (1 + std::exp(-a * (x - b)));
+}
+
+// Parabolic Curve with coefficient and offset
+double f_parabolic(double x, double a = 1, double b = 0) {
+    return a * std::pow(x, 2) + b;
+}
+
+// Cubic Curve with coefficient and offset
+double f_cubic(double x, double a = 1, double b = 0) {
+    return a * std::pow(x, 3) + b * std::pow(x, 2);
+}
+
+// Quadratic Curve with coefficients
+double f_quadratic(double x, double a = 1, double b = 0) {
+    return a * std::pow(x, 2) + b * x;
+}
+
+// Step Curve with number of steps and scale
+double f_step(double x, double a = 10, double b = 1) {
+    double step_size = 1.0 / a;
+    return b * std::floor(x / step_size) * step_size;
+}
+
+// Sensitivity Curve with sensitivity level and offset
+double f_sensitivity(double x, double a = 2.0, double b = 0) {
+    return std::pow(x, a) + b;
+}
+double f_custom(double x, double a = 2.0, double b = 0) {
+    return std::pow(x, a) + b;
+}
+
+int curveJoystick(int set_curve , int input,int max_input, double param_1, double param_2){
+  double input_norm = input/max_input;
+  switch(set_curve){
+      case linear:
+         return f_linear(input_norm, param_1, param_2);
+      case exponential:
+         return  f_exponential(input_norm, param_1, param_2);
+      case logarithmic:
+        return  f_logarithmic(input_norm, param_1, param_2);
+      case s_curve:
+         return f_s_curve(input_norm, param_1, param_2);
+      case parabolic:
+         return f_parabolic(input_norm, param_1, param_2);
+      case cubic:
+         return f_cubic(input_norm, param_1, param_2);
+      case quadratic:
+         return f_quadratic(input, param_1, param_2);
+      case step:
+         return f_step(input_norm, param_1, param_2);
+      case sensitivity:
+         return f_sensitivity(input_norm, param_1, param_2);
+      
+      default:
+          return 0;
+  }
+  /*
   if(red){
     val = (std::exp(-t/10)+std::exp((std::abs(input)-100)/10)*(1-std::exp(-t/10))) * input;
   }else{
@@ -28,6 +103,7 @@ int curveJoystick(bool red, int input, double t){
     val = std::exp(((std::abs(input)-100)*t)/1000) * input;
   }
   return val;
+  */
 }
 
 void driver(){
@@ -42,62 +118,44 @@ void driver(){
     // rightmo.spin(forward, forwardVolts - turnVolts, voltageUnits::volt);
 
     // vex::task::sleep(20);
-    if(con.ButtonL2.pressing()){
+    //double speed = curveJoystick(int set_curve , int input, double param_1, double param_2) * 100;
+    double input_axis_1=con.Axis1.position(pct);
+    double speed = curveJoystick(2 , input_axis_1,100, 0.02, 0.04) * 100;
+    /////////////////////////////////////
+    if(con.ButtonR1.pressing()){
       inta.spin(reverse, 100, pct);
-    } else if(con.ButtonL1.pressing()){
+    } else if(con.ButtonR2.pressing()){
       inta.spin(fwd, 100, pct);
     } else{
       inta.stop(hold);
     }
 
-
-    // if(con.ButtonX.pressing()){
-    //   pner.spin(reverse, 30, pct);
-    // } else if(con.ButtonB.pressing()){
-    //   pner.spin(fwd, 30, pct);
-    // } else{
-    //   pner.stop(hold);
-    // }
-
-/////////////////////////////////////////////////////////////////
-
-    //distance sensor
-    double distance = tim.objectDistance(mm);
-    if(distance < 10){
-      hood.set(false);
-    } else{
-      hood.set(true);
-    }
-
-/////////////////////////////////////////////////////////////////
-
-    //elevation button need to change
-    // if(con.ButtonDown.pressing() == true)
-    //   {
-    //     x++;
-    //   }
-    // else
-    //   {
-    //     x = 0 ;
-    //   }
-    // if(x == 1)
-    //   {
-    //     if(push == 0)
-    //     {
-    //       elevation.set(true);
-    //       push = 1;
-    //     }
-    //     else if (push == 1)
-    //     {
+      //elevation button need to change
+    if(con.ButtonL2.pressing() == true)
+      {
+        x++;
+      }
+    else
+      {
+        x = 0 ;
+      }
+    if(x == 1)
+      {
+        if(push == 0)
+        {
+          elevation.set(true);
+          push = 1;
+        }
+        else if (push == 1)
+        {
           
-    //       elevation.set(false);
+          elevation.set(false);
           
-    //       push = 0;
-    //     }
-    //   }
-
-    //clip
-    if(con.ButtonR1.pressing() == true)
+          push = 0;
+        }
+      }
+    //wing
+    if(con.ButtonL1.pressing() == true)
       {
         y++;
       }
@@ -109,12 +167,12 @@ void driver(){
       {
         if(push1 == 0)
         {
-          clip.set(true);
+          intawings.set(true);
           push1 = 1;
         }
         else if (push1 == 1)
         {
-          clip.set(false);
+          intawings.set(false);
           push1 = 0;
         }
       }
